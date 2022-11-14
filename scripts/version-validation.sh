8b819b465871211c83c4b3fcba1a2a3fcdc395ca
ts-malik.alayli <ts-malik.alayli@rakuten.com>
2022-11-14 12:26:41 +0100

    improve: Improve the version validation script (SDKCF-6040)

--- scripts/version-validation.sh
+++ scripts/version-validation.sh
@@ -6,12 +6,12 @@ if [[ -n $1 ]];
 then
     if [[ $1 =~ ^(0|[1-9][[:digit:]]*)\.(0|[1-9][[:digit:]]*)\.(0|[1-9][[:digit:]]*)$ ]];
     then
-        echo "version input is valid";
+        echo "version input is valid.";
     else
-        echo "version input is invalid";
+        echo "version input is invalid.";
         exit 1;
     fi
 else
-    echo "RELEASE_VERSION build parameter is missing";
+    echo "The version parameter (format: x.y.z) is missing.";
     exit 1;
 fi
